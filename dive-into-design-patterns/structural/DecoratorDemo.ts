30d556e75ef6dfa5b5f2dfae0a65fddef0a5c931
lijiake <lijiake@cias.cn>
2020-12-21 17:40:32 +0800

    feat(Dive Into Design Patterns): 新增Decorator模式

---
+++ dive-into-design-patterns/structural/DecoratorDemo.ts
@@ -0,0 +1,78 @@
+export interface DataSource {
+  writeData(data: string): void
+  readData(): string
+}
+
+// concrete component
+class FileDataSource implements DataSource {
+  private data: string = ''
+
+  writeData(data: string) {
+    this.data = data
+  }
+
+  readData() {
+    return this.data
+  }
+}
+
+class BaseDecorator implements DataSource {
+  protected wrapee: DataSource
+
+  constructor(component: DataSource) {
+    this.wrapee = component
+  }
+
+  writeData(data: string) {
+    this.wrapee.writeData(data)
+  }
+
+  readData() {
+    return this.wrapee.readData()
+  }
+}
+
+class Encrypt extends BaseDecorator {
+  writeData(data: string) {
+    const encrypted = data
+      .split('')
+      .map(num => `${Number(num) > 8 ? 0 : Number(num) + 1}`)
+      .join('')
+    this.wrapee.writeData(encrypted)
+  }
+
+  readData() {
+    const decrypted = this.wrapee.readData()
+      .split('')
+      .map(num => `${Number(num) < 1 ? 9 : Number(num) - 1}`)
+      .join('')
+    return decrypted
+  }
+}
+
+class Compress extends BaseDecorator {
+  writeData(data: string) {
+    this.wrapee.writeData(data.slice(4))
+  }
+
+  readData() {
+    return `0100${this.wrapee.readData()}`
+  }
+}
+
+
+const a = '01001234'
+const b = '01006789'
+
+let source: DataSource = new FileDataSource()
+let encryptSource: DataSource = new Encrypt(source)
+
+encryptSource.writeData(a)
+console.log('encrypted', source.readData())
+console.log(encryptSource.readData())
+
+
+let compressEncryptSource: DataSource = new Compress(encryptSource)
+compressEncryptSource.writeData(b)
+console.log('encrypted - compressed', source.readData())
+console.log(compressEncryptSource.readData())
